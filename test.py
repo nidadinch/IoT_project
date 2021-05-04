84cde290cc744ab15a57805a5335c1df413f6e94
Nida Dinç <20160808047qogr.akdeniz.edu.tr>
2021-05-05 01:22:52 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+
+# initialize GPIO
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BCM)
+
+# read data using pin 14
+instance = dht11.DHT11(pin=17)
+
+try:
+	while True:
+	    result = instance.read()
+	    if result.is_valid():
+	        print("Last valid input: " + str(datetime.datetime.now()))
+
+	        print("Temperature: %-3.1f C" % result.temperature)
+	        print("Humidity: %-3.1f %%" % result.humidity)
+
+	    time.sleep(6)
+
+except KeyboardInterrupt:
+    print("Cleanup")
+    GPIO.cleanup()
